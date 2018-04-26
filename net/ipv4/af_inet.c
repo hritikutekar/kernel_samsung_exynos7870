dedb451441d5cbe0b00281ed458c98456fc01ede
Luca Grifo <lg@linux.com>
2018-04-26 21:46:58 +0300

    Disable KNOX NCM

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -407,7 +407,7 @@ out_rcu_unlock:
 	rcu_read_unlock();
 	goto out;
 }
-
+#ifdef CONFIG_KNOX_NCM
 /* START_OF_KNOX_NPA */
 /** The function is used to check if the ncm feature is enabled or not; if enabled then collect the socket meta-data information; **/
 static void knox_collect_metadata(struct socket *sock) {
@@ -557,6 +557,7 @@ static void knox_collect_metadata(struct socket *sock) {
         insert_data_kfifo_kthread(ksm);
     }
 }
+#endif
 /* END_OF_KNOX_NPA */
 
 /*
@@ -590,9 +591,11 @@ int inet_release(struct socket *sock)
 		if (sock_flag(sk, SOCK_LINGER) &&
 		    !(current->flags & PF_EXITING))
 			timeout = sk->sk_lingertime;
+		#ifdef CONFIG_KNOX_NCM
         /* START_OF_KNOX_NPA */
         knox_collect_metadata(sock);
         /* END_OF_KNOX_NPA */
+		#endif
 		sock->sk = NULL;
 		sk->sk_prot->close(sk, timeout);
 	}
